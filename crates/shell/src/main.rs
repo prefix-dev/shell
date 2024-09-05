4f059505787c93574a22d19f1f541a63359c9fd3
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-09-05 17:59:48 +0200

    feat: Interactive shell (#11)

--- crates/shell/src/main.rs
+++ crates/shell/src/main.rs
@@ -1,27 +1,17 @@
-/// Entry point of the `shell` cli.
-#[tokio::main]
-async fn main() {
-    // read text of argv[1] and print it
-    let args: Vec<String> = std::env::args().collect();
-    println!("args: {:?}", args);
-    if args.len() < 2 {
-        println!("Usage: {} <script>", args[0]);
-        std::process::exit(1);
-    }
+use std::path::PathBuf;
 
-    // read text from stdin and print it
-    let script_text = std::fs::read_to_string(&args[1]).unwrap();
-    println!(
-        "Executing:\n\n{}\n\n-----------------------------------\n\n",
-        script_text
-    );
+use anyhow::Context;
+use clap::Parser;
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 
-    let list = deno_task_shell::parser::parse(&script_text).unwrap();
+async fn execute(text: &str) -> anyhow::Result<i32> {
+    let list = deno_task_shell::parser::parse(text)?;
 
     // execute
     let env_vars = std::env::vars().collect();
 
-    let cwd = std::env::current_dir().expect("Failed to get current directory");
+    let cwd = std::env::current_dir().context("Failed to get current directory")?;
 
     let exit_code = deno_task_shell::execute(
         list,
@@ -31,5 +21,69 @@ async fn main() {
     )
     .await;
 
-    std::process::exit(exit_code);
+    Ok(exit_code)
+}
+
+#[derive(Parser)]
+struct Options {
+    #[clap(short, long)]
+    file: Option<PathBuf>,
+}
+
+async fn interactive() -> anyhow::Result<()> {
+    let mut rl = DefaultEditor::new()?;
+
+    let mut prev_exit_code = 0;
+    loop {
+        // Display the prompt and read a line
+        let readline = if prev_exit_code == 0 {
+            rl.readline(">>> ")
+        } else {
+            rl.readline("xxx ")
+        };
+
+        match readline {
+            Ok(line) => {
+                // Add the line to history
+                rl.add_history_entry(line.as_str())?;
+
+                // Process the input (here we just echo it back)
+                prev_exit_code = execute(&line).await.context("Failed to execute")?;
+
+                // Check for exit command
+                if line.trim().eq_ignore_ascii_case("exit") {
+                    println!("Exiting...");
+                    break;
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let options = Options::parse();
+
+    if let Some(file) = options.file {
+        let script_text = std::fs::read_to_string(&file).unwrap();
+        execute(&script_text).await?;
+    } else {
+        interactive().await?;
+    }
+
+    Ok(())
 }
