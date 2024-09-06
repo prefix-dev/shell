d92035d507adc8f8822bbb404836263e64b4fc02
prsabahrami <p.bahraminejad@gmail.com>
2024-09-06 12:29:55 -0400

    Fix exit on syntax error

--- crates/shell/src/main.rs
+++ crates/shell/src/main.rs
@@ -21,15 +21,24 @@ fn commands() -> HashMap<String, Rc<dyn ShellCommand>> {
 }
 
 async fn execute(text: &str, state: &mut ShellState) -> anyhow::Result<i32> {
-    let list = deno_task_shell::parser::parse(text)?;
+    let list = deno_task_shell::parser::parse(text);
+
+    let mut stderr = ShellPipeWriter::stderr();
+    let stdout = ShellPipeWriter::stdout();
+    let stdin = ShellPipeReader::stdin();
+
+    if let Err(e) = list {
+        let _ = stderr.write_line(&format!("Syntax error: {}", e));
+        return Ok(1);
+    }
 
     // spawn a sequential list and pipe its output to the environment
     let result = execute_sequential_list(
-        list,
+        list.unwrap(),
         state.clone(),
-        ShellPipeReader::stdin(),
-        ShellPipeWriter::stdout(),
-        ShellPipeWriter::stderr(),
+        stdin,
+        stdout,
+        stderr,
         AsyncCommandBehavior::Wait,
     )
     .await;
