891c239a2de76e99d20ec9846253d9ef0bd4337c
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-09-08 08:56:42 -0400

    print last line when cat-ting interactively

--- crates/deno_task_shell/src/shell/commands/cat.rs
+++ crates/deno_task_shell/src/shell/commands/cat.rs
@@ -3,9 +3,11 @@
 use anyhow::Result;
 use futures::future::LocalBoxFuture;
 use std::fs::File;
+use std::io::IsTerminal;
 use std::io::Read;
 
 use crate::shell::types::ExecuteResult;
+use crate::ShellPipeWriter;
 
 use super::args::parse_arg_kinds;
 use super::args::ArgKind;
@@ -55,6 +57,14 @@ fn execute_cat(mut context: ShellCommandContext) -> Result<ExecuteResult> {
             break;
           } else {
             context.stdout.write_all(&buf[..size])?;
+          }
+
+          if let ShellPipeWriter::Stdout = context.stdout {
+            // check if it's interactive
+            if buf[size - 1] != b'\n' && std::io::stdout().is_terminal() {
+              // make sure that we end up on a new line
+              context.stdout.write_all(b"%\n")?;
+            }
           }
         },
         Err(err) => {
