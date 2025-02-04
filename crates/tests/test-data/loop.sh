8df90551f19ada4b3e570d374e089ced54a4fae4
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-02-04 13:11:14 +0100

    add additional tests

--- crates/tests/test-data/loop.sh
+++ crates/tests/test-data/loop.sh
@@ -16,3 +16,16 @@ Current fruit: orange
 >     echo "Current fruit: $item"
 > done
 Current fruit: apple banana orange
+
+# test single line for loop
+> for item in "apple" "banana" "orange"; do echo "Current fruit: $item"; done
+Current fruit: apple
+Current fruit: banana
+Current fruit: orange
+
+# > for i in {1..5}; do echo $i; done
+# 1
+# 2
+# 3
+# 4
+# 5
